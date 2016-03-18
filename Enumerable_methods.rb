8a3503a95b2f6f886db405424fbfa1e34c098065
K.Stepanov <vostok@vostok>
2016-03-18 05:34:55 +0200

    Edit my_all?

--- Enumerable_methods.rb
+++ Enumerable_methods.rb
@@ -4,18 +4,12 @@ module Enumerable
       yield i if block_given?
     end
   end
-#[1,131,312,2].my_each {|x| puts x}
 
   def my_each_with_index
     for item, index in collect()
       yield item, index if block_given?
     end
   end
-# hash = Hash.new
-# %w(cat dog wombat).each_with_index { |item, index|
-# hash[item] = index
-# }
-#hash
 
   def my_select
     for i in collect()
@@ -28,13 +22,10 @@ module Enumerable
   def my_all?
     for i in collect()
       if block_given?
-        while yield i == true
-        end
+        return yield i if false
       end
     end
   end
-
-# [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
 
 
 end
